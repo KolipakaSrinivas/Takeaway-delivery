56137d1ceb697098f0e1b9602aae275a2b414c49
Srinivas <kolipakasrinivas456@gmail.com>
2023-09-04 19:09:00 +0530

    added  feature when click on menu berger icon it open

---
+++ javascript.js
@@ -0,0 +1,23 @@
+const menutoggle = ()=> document.body.classList.toggle('open')
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
