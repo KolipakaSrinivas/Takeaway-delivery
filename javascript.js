fbb92f736e80603e75bf49ca27b6b29f11580c74
Srinivas <kolipakasrinivas456@gmail.com>
2023-09-13 22:08:48 +0530

    add cart html file and logo icon address bar

--- javascript.js
+++ javascript.js
@@ -2,22 +2,22 @@ const menutoggle = ()=> document.body.classList.toggle('open')
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const navLinks = document.querySelectorAll('nav ul li a');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const navLinks = document.querySelectorAll('nav ul li a');
 
-    navLinks.forEach(function (link) {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
+//     navLinks.forEach(function (link) {
+//         link.addEventListener('click', function (e) {
+//             e.preventDefault();
 
-            const targetId = this.getAttribute('href').substring(1);
-            const targetSection = document.getElementById(targetId);
+//             const targetId = this.getAttribute('href').substring(1);
+//             const targetSection = document.getElementById(targetId);
 
-            if (targetSection) {
-                window.scrollTo({
-                    top: targetSection.offsetTop,
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
-});
+//             if (targetSection) {
+//                 window.scrollTo({
+//                     top: targetSection.offsetTop,
+//                     behavior: 'smooth'
+//                 });
+//             }
+//         });
+//     });
+// });
